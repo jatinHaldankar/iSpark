3b66bcee212dc2611734070b8a5304e059144d40
jatinHaldankar <127951422+jatinHaldankar@users.noreply.github.com>
2023-03-15 17:50:10 +0530

    utilities added

---
+++ utilities/footer.php
@@ -0,0 +1,12 @@
+
+  <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top" style="height:4vh">
+    <p class="col-md-4 mb-0 text-muted" style="font-weight: bold;">© 2023 iSpark Bank, Inc</p>
+
+    <div class="col-md-4 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-dark text-decoration-none">
+      <img src="images/logo.png" style="height:10vh" alt="" srcset="">
+    </div>
+
+    
+    <p class="col-md-3 mb-0 text-muted" style="text-align: end;  font-weight: bold;">Created By:Jatin Haldankar</p>
+     
+  </footer>
